829f978595924a1711b48d3c66a0ce5aa2cee002
Cristian Cundari <cristian3095@hotmail.it>
2025-03-30 11:22:23 +0200

    fix apos

--- components/impostazioni/GoogleCalendarLink.tsx
+++ components/impostazioni/GoogleCalendarLink.tsx
@@ -7,7 +7,7 @@ function GoogleCalendarLink({ disabled }: { disabled: boolean }) {
   return (
     <>
       <Title order={5}>Collega il tuo account google calendar</Title>
-      <Text size="sm">Effettuando l'accesso potrai sincronizzare i promemoria creati in-app sul tuo calendario google</Text>
+      <Text size="sm">Effettuando l&apos;accesso potrai sincronizzare i promemoria creati in-app sul tuo calendario google</Text>
       <Button component={Link} href="/api/google/action" disabled={disabled} mt="md" leftSection={<IconBrandGoogle />}>
         Collega Google Calendar
       </Button>
